0103093a7df3067517cdcdc25d8911ad652f89f8
Nikita <number_nik@mail.ru>
2022-12-21 12:43:20 +0300

    Решение ДЗ 1 Java OOP

--- DZ/Dz_1/HotDrinkVendingMachine.java
+++ DZ/Dz_1/HotDrinkVendingMachine.java
@@ -23,58 +23,74 @@ public class HotDrinkVendingMachine {
         product.setTemperature(temperature);
         return product;
     }
-    // реализация меню
+
+
     public static String[] menu () {
-        System.out.println("Выберите напиток:\n" +
+        System.out.println("Выберите газировку:\n" +
                             "1 - Colla\n" +
                             "2 - Fanta\n" +
                             "3 - Sprite\n" +
                             "4 - Merinda\n");
         Scanner sk = new Scanner(System.in);
-        int choice = sk.nextInt();
+        int soda = sk.nextInt();
 
-        System.out.println("Желаемый объем напитка:\n" +
+        System.out.println("Желаемый объем газировки:\n" +
                             "1 - 0.5л\n" +
                             "2 - 1.0л\n");
         int volum = sk.nextInt();
 
-        System.out.println("Выберите температуоу напитка:\n" +
+        System.out.println("Выберите температуру газировки:\n" +
                                 "1:  -5°\n" + 
-                                "2:  0°\n" + 
-                                "3:  5°\n"); 
+                                "2:   0°\n" + 
+                                "3:   5°\n"); 
         int temp = sk.nextInt();
 
-        String[] prod = new String [3];
-        for (int i = 0; i < prod.length; i++) {
-            if (choice == 1) {
-                prod[0] = "чай";
-            } else if (choice == 2) {
-                prod[0] = "кофе";
-            } else if (choice == 3) {
-                prod[0] = "кофе с молоком";
-            } else if (choice == 4) {
-                prod[0] = "какао";
+        String[] prod = new String[3];
+        for (int i = 0; i < prod.length; i++) 
+            {
+                if (soda == 1) 
+                    {
+                        prod[0] = "Colla";
+                    } 
+                else if (soda == 2) 
+                    {
+                        prod[0] = "Fanta";
+                    } 
+                else if (soda == 3) 
+                    {
+                        prod[0] = "Sprite";
+                    } 
+                else if (soda == 4) 
+                    {
+                        prod[0] = "Merinda";
+                    }
+
+
+
+                if (volum == 1) 
+                    {
+                        prod[1] = "0.5";
+                    } 
+                else if (volum == 2) 
+                    {
+                        prod[1] = "1.0";
+                    }
+
+
+                
+                if (temp == 1) 
+                    {
+                        prod[2] = "-5°";
+                    } 
+                else if (temp == 2) 
+                    {
+                        prod[2] = " 0°";
+                    } 
+                else if (temp == 3) 
+                    {
+                        prod[2] = " 5°";
+                    }
             }
-            else if (choice == 5) {
-                prod[0] = "горячий шоколад";
-            }
-            if (volum == 1) {
-                prod[1] = "0.2";
-            } else if (volum == 2) {
-                prod[1] = "0.5";
-            } else if (volum == 3) {
-                prod[1] = "0.7";
-            }
-            if (temp == 1) {
-                prod[2] = "70";
-            } else if (temp == 2) {
-                prod[2] = "75";
-            } else if (temp == 3) {
-                prod[2] = "80";
-            } else if (temp == 4) {
-                prod[2] = "85";
-            }
-        }
         return prod;
     }
 
--- DZ/Dz_1/Main.java
+++ DZ/Dz_1/Main.java
@@ -4,16 +4,19 @@ public class Main {
     public static void main(String[] args) {
         String[] product = HotDrinkVendingMachine.menu(); 
         
-        for (String item : product) {
-            System.out.println(item);
-        }
+        for (String item : product) 
+            {
+                System.out.println(item);
+            }
         
         HotDrinkTemperature product1 = new HotDrinkTemperature(product[0], Double.parseDouble(product[1]), Double.parseDouble(product[2]));
         HotDrinkVendingMachine product2 = new HotDrinkVendingMachine(product1);
         System.out.println(product1);
         System.out.println(product2);
-        // меняем экземпляр класса vending.. с помощью перегруженного метода
-        product2.getProduct("чай", 0.2, 85);
+
+
+        product2.getProduct("mojito", 0.5, -5);
         System.out.println(product2);
+        
     }
 }
